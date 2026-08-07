8be12af2e40f03c2931450de2de4ff3ae0e28d94
Eduardo Souza <edusoliveira24@gmail.com>
2026-08-07 15:01:42 -0300

    refactor: menu com switch e funcao separada

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+while (true)
+{
+    ExibirMenu();
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+            Console.WriteLine("Funcionalidade em desenvolvimento...");
+            break;
+        case "5":
+            Console.WriteLine("Obrigado por usar o MiniBank. Até logo!");
+            return;
+
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+}
+
+static void ExibirMenu()
+{
+    Console.WriteLine("======================================================");
+    Console.WriteLine("          MINI BANK - SEU BANCO DIGITAL");
+    Console.WriteLine("======================================================");
+
+    Console.WriteLine("1. Criar conta");
+    Console.WriteLine("2. Depositar");
+    Console.WriteLine("3. Sacar");
+    Console.WriteLine("4. Ver saldo");
+    Console.WriteLine("5. Sair");
+
+    Console.WriteLine("Escolha uma opção: ");
+}
